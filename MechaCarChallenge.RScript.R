a3d5589aed44ae5f3d38da1121d28fe0966043f5
Simon Adeoye <mymac@Simons-MacBook-Pro.local>
2021-06-13 19:22:22 -0400

    include

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,29 @@
-MechaCar_table <- read.csv(file = 'MechaCar_mpg.csv')
-mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table)
-lm(MechaCar_table)
-summary(lm(, data = MechaCar_table))
+# Deliverable 1
+
+library(dplyr)
+
+car_tb <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=car_tb)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=car_tb)
+)
+
+# Deliverable 2
+
+sus_tb <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- sus_tb %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+lot_summary <- sus_tb %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Deliverable 3
+
+t.test(sus_tb$PSI, mu=1500)
+
+t.test(subset(sus_tb$PSI, sus_tb$Manufacturing_Lot == "Lot1"), mu=1500)
+
+t.test(subset(sus_tb$PSI, sus_tb$Manufacturing_Lot == "Lot2"), mu=1500)
+
+t.test(subset(sus_tb$PSI, sus_tb$Manufacturing_Lot == "Lot3"), mu=1500)
+
